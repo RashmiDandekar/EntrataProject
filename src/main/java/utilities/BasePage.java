60343894711d33d4981f2c95d3cee2660d4f9b27
RashmiDandekar <72007914+RashmiDandekar@users.noreply.github.com>
2026-01-31 19:28:12 +0530

    Update BasePage.java

--- src/main/java/utilities/BasePage.java
+++ src/main/java/utilities/BasePage.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.URL;
 
 public class BasePage {
 	public static WebDriver driver;
